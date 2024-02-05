ba20d3d054bc4f5913e6afc1ca958fed8ddb0e28
Roberto Correa <robertoracm2007@gmail.com>
2024-02-04 21:10:07 -0500

    Commit Principal

---
+++ src/architecture-test/java/cl/bci/architecture/ArchitectureSuiteTest.java
@@ -0,0 +1,9 @@
+package cl.bci.architecture;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@SelectPackages({"cl.bci.architecture.rules"})
+@Suite
+class ArchitectureSuiteTest {
+}
---
+++ src/main/java/EjemploGradleArch/cl/bci/ecodig/customersupport/customerprofile/controller/pruebaConf.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.bci.ecodig.customersupport.customerprofile.controller;
+
+/**
+ *
+ * @author USER
+ */
+public class pruebaConf {
+    
+}
