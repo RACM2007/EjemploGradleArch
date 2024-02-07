41e1b89edec0ce4fc93868f86ab89e8ad118fc3b
Roberto Correa <robertoracm2007@gmail.com>
2024-02-06 21:33:44 -0500

    commit

--- src/architecture-test/java/cl/bci/architecture/ArchitectureSuiteTest.java
+++
@@ -1,9 +0,0 @@
-package cl.bci.architecture;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@SelectPackages({"cl.bci.architecture.rules"})
-@Suite
-class ArchitectureSuiteTest {
-}
